cc085de4e6922b59ccffbb15fd065913509f1337
w282184872 <w282184872@users.noreply.github.com>
2026-09-01 06:18:24 +0800

    fix: 锁定 helloworld 与全部 Action 版本，启用 luci-ssl 与 update-checker 定时调度，同步 preinit IP

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld^9747ffd550052fc7c9237853e4666122bdba9e5b' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
