3b696b3795caa3b395eebbb3fff90c3943237f3a
w282184872 <35095479+w282184872@users.noreply.github.com>
2026-09-01 05:54:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,10 @@ sed -i "s/hostname='ImmortalWrt'/hostname='$ROUTER_NAME'/g" package/base-files/f
 # 3. 修改默认主题 (可选)
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 4. 强制升级 Golang 版本到 1.26 (精准指定分支以修复 xray-core 编译报错)
+# 4. 强制升级 Golang 版本到 1.26 (修复 xray-core 编译报错及目录结构层级问题)
 rm -rf feeds/packages/lang/golang
 git clone -b 1.26 --depth 1 https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 将嵌套的子目录文件全部提起到当前根目录，与 .mk 文件同级
+cp -af feeds/packages/lang/golang/golang/* feeds/packages/lang/golang/
+rm -rf feeds/packages/lang/golang/golang
