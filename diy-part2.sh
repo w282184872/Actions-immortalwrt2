0028414100766305064d54214826977f7b34225a
w282184872 <35095479+w282184872@users.noreply.github.com>
2026-09-01 19:03:06 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,15 +15,16 @@ sed -i "s/hostname='ImmortalWrt'/hostname='$ROUTER_NAME'/g" package/base-files/f
 # 3. 修改默认主题 (可选)
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 4. 强制升级 Golang 版本到 1.26
-rm -rf feeds/packages/lang/golang
-git clone -b 1.26 --depth 1 https://github.com/kenzok8/golang feeds/packages/lang/golang
-# 将嵌套的子目录文件全部提起到当前根目录，与 .mk 文件同级
-cp -af feeds/packages/lang/golang/golang/* feeds/packages/lang/golang/
-rm -rf feeds/packages/lang/golang/golang
+# 4. 修复 xray-core 编译报错 (终极无冲突方案)
+# 清除 feeds 中所有的 xray-core 及其潜在干扰
+find feeds/ -name "xray-core" -type d -exec rm -rf {} +
 
-# 5. 修复 xray-core 编译报错 (替换为兼容高版本 Golang 的新版 xray-core)
-rm -rf feeds/packages/net/xray-core
-git clone --depth 1 https://github.com/immortalwrt/packages.git /tmp/packages
-cp -r /tmp/packages/net/xray-core feeds/packages/net/
-./scripts/feeds install xray-core
+# 从 immortalwrt 23.05 稳定分支拉取 xray-core (完美兼容当前的 Golang 1.24)
+git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/packages.git /tmp/packages
+
+# 将其移动到 package/custom 目录下，确保最高编译优先级
+mkdir -p package/custom
+cp -r /tmp/packages/net/xray-core package/custom/
+
+# 修复 Makefile 中的相对路径，将其指向正确的 feeds/packages/lang/ 目录
+sed -i 's|../../lang/|$(TOPDIR)/feeds/packages/lang/|g' package/custom/xray-core/Makefile
