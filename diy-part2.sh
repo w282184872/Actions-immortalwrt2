a91a934b521d60ea62ae28a27d95857e95f5a9e6
w282184872 <35095479+w282184872@users.noreply.github.com>
2026-09-02 00:09:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,17 +12,17 @@ sed -i "s/192.168.1.1/$ROUTER_IP/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='OpenWrt'/hostname='$ROUTER_NAME'/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='$ROUTER_NAME'/g" package/base-files/files/bin/config_generate
 
-# 3. 修改默认主题 (可选)
+# 3. 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 4. 修复 xray-core 编译报错 (终极版本号篡改法，绝对不冲突)
-# 确保清理掉之前乱加的 custom 目录（如果在 Actions 里有残留的话）
-rm -rf package/custom/xray-core
+# 4. 升级 Golang 编译器 (采用 sbwml 大佬专门为 21.02 适配的 Go 1.23，绝对稳定)
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
-# 直接修改自带源码的版本号，从有 bug 的 1.8.3 升级到完美兼容老 Go 编译器的 1.8.24
+# 5. 将自带的 xray-core 升级为 1.8.24 (完美匹配 Go 1.23，解决 gvisor 报错)
 sed -i 's/PKG_VERSION:=1.8.3/PKG_VERSION:=1.8.24/g' feeds/packages/net/xray-core/Makefile
 sed -i 's/PKG_VERSION:=1.8.1/PKG_VERSION:=1.8.24/g' feeds/packages/net/xray-core/Makefile
 sed -i 's/PKG_VERSION:=1.8.4/PKG_VERSION:=1.8.24/g' feeds/packages/net/xray-core/Makefile
 
-# 强行跳过官方源码包的 SHA256 哈希值校验 (因为版本号变了，下载包的哈希也会变)
+# 强行跳过官方源码包的 SHA256 哈希值校验
 sed -i 's/PKG_HASH:=.*/PKG_HASH:=skip/g' feeds/packages/net/xray-core/Makefile
